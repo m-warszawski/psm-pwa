6cb8396d2eec00b777550d94796c4a633500d0da
s-121-16 <s-121-16@gumiforest.ae.krakow.pl>
2024-04-17 12:31:39 +0200

    bootstrap

--- js/main.js
+++ js/main.js
@@ -9,6 +9,48 @@ window.onload = () => {
 
 function sosVibration() {
   window.navigator.vibrate([ 
-    30,30,30,100,100,100,30,30,30
+    200
   ]);
+  setTimeout(150);
+  window.navigator.vibrate([ 
+    200
+  ]);
+  setTimeout(150);
+  window.navigator.vibrate([ 
+    200
+  ]);
+  setTimeout(150);
+  window.navigator.vibrate([ 
+    600
+  ]);
+  setTimeout(300)
+  window.navigator.vibrate([ 
+    600
+  ]);
+  setTimeout(300)
+  window.navigator.vibrate([ 
+    600
+  ]);
+  setTimeout(300)
+  window.navigator.vibrate([ 
+    200
+  ]);
+  setTimeout(150);
+  window.navigator.vibrate([ 
+    200
+  ]);
+  setTimeout(150);
+  window.navigator.vibrate([ 
+    200
+  ]);
+  
 }
+
+function getCoordinates(){
+  navigator.geolocation.getCurrentPosition(function(location) {
+    console.log(location.coords.latitude);
+    console.log(location.coords.longitude);
+    console.log(location.coords.accuracy);
+  });
+}
+
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var cacheName = 'hello-pwa';
+var cacheName = 'psm-pwa';
 var filesToCache = [
   '/',
   '/index.html',
