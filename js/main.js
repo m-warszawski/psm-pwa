a5a0b7b6d87085905436de04fec840a392405542
s-121-16 <s-121-16@gumiforest.ae.krakow.pl>
2024-04-17 13:05:27 +0200

    update

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@ function sosVibration() {
 // MAP
 var mapOptions = {
   center: [50.06914, 19.95470],
-  zoom: 10
+  zoom: 15
 }
 
 var map = new L.map('area', mapOptions);
@@ -37,3 +37,16 @@ var layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
 
 map.addLayer(layer);
 console.log(location)
+
+var markerOptions = {
+  title: "MyLocation",
+  clickable: true,
+  draggable: true,
+}
+var marker = L.marker([17.438139, 78.395830], markerOptions);
+         
+         // Adding popup to the marker
+marker.bindPopup('Hi welcome to Tutorialspoint').openPopup();
+         
+         // Adding marker to the map
+marker.addTo(map);
