38f6dbbb67273d3ad5368531e65282fc0303f2c9
s-121-16 <s-121-16@gumiforest.ae.krakow.pl>
2024-04-17 13:02:36 +0200

    update

--- js/main.js
+++ js/main.js
@@ -3,31 +3,37 @@ window.onload = () => {
 
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-             .register('./sw.js');
+      .register('./sw.js');
   }
 }
 
+// SOS
 function sosVibration() {
-  
-  setTimeout(window.navigator.vibrate(2000),1500);
-  setTimeout(window.navigator.vibrate(2000),1500);
-  setTimeout(window.navigator.vibrate(2000),1500);
 
-  setTimeout(window.navigator.vibrate(6000),1500);
-  setTimeout(window.navigator.vibrate(6000),1500);
-  setTimeout(window.navigator.vibrate(6000),1500);
+  setTimeout(window.navigator.vibrate(2000), 1500);
+  setTimeout(window.navigator.vibrate(2000), 1500);
+  setTimeout(window.navigator.vibrate(2000), 1500);
 
-  setTimeout(window.navigator.vibrate(2000),1500);
-  setTimeout(window.navigator.vibrate(2000),1500);
-  setTimeout(window.navigator.vibrate(2000),1500);
-  
+  setTimeout(window.navigator.vibrate(6000), 1500);
+  setTimeout(window.navigator.vibrate(6000), 1500);
+  setTimeout(window.navigator.vibrate(6000), 1500);
+
+  setTimeout(window.navigator.vibrate(2000), 1500);
+  setTimeout(window.navigator.vibrate(2000), 1500);
+  setTimeout(window.navigator.vibrate(2000), 1500);
+
 }
 
-function getCoordinates(){
-  navigator.geolocation.getCurrentPosition(function(location) {
-    console.log(location.coords.latitude);
-    console.log(location.coords.longitude);
-    console.log(location.coords.accuracy);
-  });
+
+// MAP
+var mapOptions = {
+  center: [50.06914, 19.95470],
+  zoom: 10
 }
 
+var map = new L.map('area', mapOptions);
+
+var layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+
+map.addLayer(layer);
+console.log(location)
