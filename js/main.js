71a8ab3b96888181436d0459bdcbf7a40c88caeb
Michał <s223194@student.uek.krakow.pl>
2024-04-18 14:14:28 +0200

    code refactoring

--- js/main.js
+++ js/main.js
@@ -8,23 +8,23 @@ window.onload = () => {
 }
 
 // SOS
- function sosVibration() {
-window.navigator.vibrate([600,600,600,600,600,600,1500,600,1500,600,1500,600,600,600,600,600,600]);
+function sosVibration() {
+  window.navigator.vibrate([600, 600, 600, 600, 600, 600, 1500, 600, 1500, 600, 1500, 600, 600, 600, 600, 600, 600]);
 }
 
-//map
-function  renderMap(){
+// MAP
+function renderMap() {
   var mapOptions = {
     center: [50.049683, 19.944544],
     zoom: 10
-  }
-  
+  };
+
   // Creating a map object
-  var map = new L.map('map', mapOptions);
-  
+  var map = new L.map('area', mapOptions);
+
   // Creating a Layer object
   var layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
-  
+
   // Adding layer to the map
   map.addLayer(layer);
 
@@ -33,21 +33,23 @@ function  renderMap(){
     var radius = e.accuracy / 2;
 
     var locationMarker = L.marker(e.latlng).addTo(map)
-        .bindPopup("You are somewhere within this circle").openPopup();
+      .bindPopup("You are somewhere within this circle").openPopup();
 
     var locationCircle = L.circle(e.latlng, radius).addTo(map);
     map.setView(e.latlng, 13); // Zoom closer
-    }
+  }
 
   function onLocationError(e) {
-    alert(e.message);
+    alert("Could not retrieve your location.");
+  }
+
+  function onLocateError(err) {
+    alert("Could not retrieve your location.");
   }
 
   map.on('locationerror', onLocationError);
   map.on('locationfound', onLocationFound);
 
   // Request user's location
-  map.locate({setView: true, maxZoom: 16});
+  map.locate({ setView: true, maxZoom: 16 }).on('error', onLocateError);
 }
-
-
